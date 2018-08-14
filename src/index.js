b65c04436f057cb937d4e3fcf0a66d450419a788
BeagleDad1 <formatt.hoffman@gmail.com>
2018-08-14 07:29:56 -0700

    removed api key value for security reasons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import _ from 'lodash';
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
-const API_KEY = 'AIzaSyAaQYQwwjCVc4FDRGSELCjQqg0x4U8HHhQ';
+const API_KEY = '/home/matthew/dev/yt_api_key.txt';
 
 
 
@@ -12,17 +14,33 @@ const API_KEY = 'AIzaSyAaQYQwwjCVc4FDRGSELCjQqg0x4U8HHhQ';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-      this.setState({videos});
+    this.videoSearch('surfboards');
+}
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
